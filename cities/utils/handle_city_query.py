7fd09f510028c4424cdd150575ff5fe664b46401
Dmitry <dmitry.dev@protonmail.com>
2022-03-27 17:20:55 +0900

    Move getting cities queryset to utils

--- cities/utils/handle_city_query.py
+++ cities/utils/handle_city_query.py
@@ -1,11 +1,11 @@
+from django.db.models import QuerySet
+
 from cities.models import City
 from cities.services.coordinates import get_distance
 
 
-def get_closest_cities_qs(params) -> list[int] | None:
+def get_closest_cities_ids(requested_lat: float, requested_lon: float) -> list[int] | None:
     cities = City.objects.filter(latitude__isnull=False, longitude__isnull=False)
-    requested_lat = float(params['lat']) if params.get('lat') else None
-    requested_lon = float(params['lon']) if params.get('lon') else None
     if requested_lat is None or requested_lon is None:
         return None
     distance_to_cities = []
@@ -19,3 +19,7 @@ def get_closest_cities_qs(params) -> list[int] | None:
         )
     sorted_cities_ids = list(map(lambda d: d['id'], sorted(distance_to_cities, key=lambda d: d['distance'])))
     return sorted_cities_ids[:2]
+
+
+def get_cities_qs(cities_closest_ids: list[int] | None = None) -> QuerySet:
+    return City.objects.filter(id__in=cities_closest_ids) if cities_closest_ids else City.objects.all()
