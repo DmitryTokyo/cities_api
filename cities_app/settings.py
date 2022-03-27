705132bedccda53ab544843378091f87efcb4be8
Dmitry <dmitry.dev@protonmail.com>
2022-03-27 23:35:44 +0900

    Fix default allow host

--- cities_app/settings.py
+++ cities_app/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = env.list('ALLOW_HOSTS', default='127.0.0.1')
+ALLOWED_HOSTS = env.list('ALLOW_HOSTS', default='0.0.0.0')
 
 
 INSTALLED_APPS = [
